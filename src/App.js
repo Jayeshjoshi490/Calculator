03efcf3c39344e8ccc9f6195521e7b9f54fedcc6
Jayesh Joshi <jayeshjoshi490@gmail.com>
2022-01-06 21:07:49 +0530

    New Calculator

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react'
+import './App.css'
+import Calculator from './component/Calculator'
+
+function App() {
+  return (
+    <Calculator />
+  )
+}
+
+export default App
+
